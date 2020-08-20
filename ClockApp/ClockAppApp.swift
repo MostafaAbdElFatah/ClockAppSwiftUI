d6b56016461e58b5bf56c47c5ef0670653dd227e
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2020-08-20 12:23:28 +0200

    Initial Commit

---
+++ ClockApp/ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by Mostafa Abd ElFatah on 8/20/20.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClockApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClockApp
+//
+//  Created by Mostafa Abd ElFatah on 8/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
